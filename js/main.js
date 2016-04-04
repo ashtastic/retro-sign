6c9cdbc4dd6be87297a95425a646a6517dedc59f
ashtastic <amurr20@gmail.com>
2016-04-04 16:27:59 -0400

    Added class properties to turn engage off and on.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var $btn = $('.btn');
+var $svg = $('svg');
+
+$('.btn').on('click', function () {
+  if ($svg.hasClass('is-engaged')) {
+    $svg.addClass('is-disengaged');
+    $svg.removeClass('is-engaged');
+  } else {
+    $svg.addClass('is-engaged');
+    $svg.removeClass('is-disengaged');
+  }
+});
+
